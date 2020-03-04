eca390c1b0b3c71c1162a300748a40e9faaff1b0
kadartibi <kadart78@gmail.com>
2020-03-04 14:39:14 +0100

    added new trip form

---
+++ src/components/AddNewTripForm.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: "100px",
+    margin: "auto",
+    maxWidth: "50%",
+    minWidth: "300",
+    textAlign: "center"
+  },
+  content: {
+    display: "inline-block"
+  }
+});
+
+export const AddNewTripForm = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <React.Fragment>
+        <Card className={classes.root}>
+          <CardContent className={classes.content}>
+            <form noValidate autoComplete="off">
+              <TextField
+                className="tripInput"
+                id="standard-basic"
+                label="Trip name"
+              />
+              <br />
+              <TextField
+                className="tripInput"
+                id="standard-basic"
+                label="Country"
+              />
+              <br />
+              <TextField
+                className="tripInput"
+                id="standard-basic"
+                label="City"
+              />
+              <br />
+              <TextField
+                className="tripInput"
+                id="standard-basic"
+                label="Traveltype"
+              />
+              <br />
+            </form>
+          </CardContent>
+          <CardActions className={classes.action}>
+            <Button size="small">Submit trip</Button>
+          </CardActions>
+        </Card>
+      </React.Fragment>
+    </div>
+  );
+};
