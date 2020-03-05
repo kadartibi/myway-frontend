3e0af1966ab869c2ca334748d440208a1d57e94b
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-03-05 08:27:09 +0100

    add local server for development

--- src/components/Context/TripContext.js
+++ src/components/Context/TripContext.js
@@ -4,26 +4,11 @@ import Axios from 'axios';
 export const TripContext = createContext();
 
 export const TripProvider = props => {
-    // const trips = [{
-    //     "name": "Spanish dream",
-    //     "country": "Spain",
-    //     "dateOfDeparture": "10/02/2020",
-    //     "dateOfReturn" : "15/02/2020",
-    //     "totalCost" : 200,
-    //     "rating": 3
-    // },
-    // {
-    //     "name": "Spanish dream2",
-    //     "country": "Spain",
-    //     "dateOfDeparture": "10/02/2020",
-    //     "dateOfReturn" : "25/02/2020",
-    //     "totalCost" : 500,
-    //     "rating": 6
-    // }];
 
     const [trips, setTrips] = useState([]);
 
     useEffect(() => {
+        // Axios.get("http://10.44.1.211:8080/trip/list").then(res => {
         Axios.get("http://localhost:8080/trip/list").then(res => {
             setTrips(res.data)
             console.log(res.data)
