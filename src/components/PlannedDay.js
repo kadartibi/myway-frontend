cf40b7db495442a941b8eeeb11d941efc862efa0
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-03-19 22:34:27 +0100

    fix warning and add header for new trip page

--- src/components/PlannedDay.js
+++ src/components/PlannedDay.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import CardActions from "@material-ui/core/CardActions";
