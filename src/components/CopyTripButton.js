479665ddeee6b9b4df71cfab0ef817be64be1275
Armin Kovacs <armin0191@gmail.com>
2020-05-19 11:55:13 +0200

    Add dialog box for date picking when copying trip

--- src/components/CopyTripButton.js
+++ src/components/CopyTripButton.js
@@ -2,18 +2,38 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
 import CloseIcon from "@material-ui/icons/Close";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 
 const useStyles = makeStyles({
   button: {
-    marginBottom: 20
+    marginBottom: 20,
   },
 });
 
 export default function CopyTripButton(props) {
   const [open, setOpen] = React.useState(false);
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+
+  const [openDialog, setOpenDialog] = React.useState(false);
+
+  const handleClickOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
   const classes = useStyles();
 
   const handleClick = () => {
@@ -24,19 +44,15 @@ export default function CopyTripButton(props) {
     if (reason === "clickaway") {
       return;
     }
-
     setOpen(false);
   };
 
   const sendToServer = (props) => {
-    console.log(props.trip.id)
+    console.log(props.trip.id);
     axios
-      .get(
-        "http://localhost:8762/trip/copy-trip/" + props.trip.id,
-        {
-          withCredentials: true,
-        }
-      )
+      .get("http://localhost:8762/trip/copy-trip/" + props.trip.id, {
+        withCredentials: true,
+      })
       .then((response) => {
         console.log(response);
       })
@@ -52,12 +68,43 @@ export default function CopyTripButton(props) {
         variant="outlined"
         size="small"
         onClick={() => {
-          sendToServer(props);
-          handleClick();
+          handleClickOpenDialog();
         }}
       >
         Copy trip
       </Button>
+      <Dialog
+        open={openDialog}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleCloseDialog}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Change date
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Disagree
+          </Button>
+          <Button
+            onClick={() => {
+              sendToServer(props);
+              handleCloseDialog();
+              handleClick();
+            }}
+            color="primary"
+          >
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
       <Snackbar
         anchorOrigin={{
           vertical: "bottom",
@@ -65,16 +112,20 @@ export default function CopyTripButton(props) {
         }}
         open={open}
         autoHideDuration={3000}
-        onClose={handleClose}
+        onClose={handleCloseDialog}
         message="Trip copied"
         action={
           <React.Fragment>
-            <Button color="secondary" size="small" onClick={handleClose}/>
+            <Button
+              color="secondary"
+              size="small"
+              onClick={handleCloseDialog}
+            />
             <IconButton
               size="small"
               aria-label="close"
               color="inherit"
-              onClick={handleClose}
+              onClick={handleCloseDialog}
             >
               <CloseIcon fontSize="small" />
             </IconButton>
